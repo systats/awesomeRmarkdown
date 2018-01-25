24bbb526d57d59035c521e9a370c59d0d2db0744
Simon Roth <systats@users.noreply.github.com>
2018-01-25 01:09:09 +0100

    new structure

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 awesomeRmarkdown
 ================
 
+-   [Some Information](#some-information)
+
+Some Information
+================
+
+Please visit the corresponding [website](https://systats.github.io/awesomeRmarkdown). There you find a live version of this Rmarkdown Website hosted on github as well as additional information on Rmarkdown.
+
 | x     | y     |
 |:------|:------|
 | hello | world |
--- helpers.R
+++ helpers.R
@@ -68,45 +68,45 @@ repo_structure <- function(){
 
 sidebar <- function() {
   tagList(
-  div(class="ui sidebar inverted vertical visible menu",
-    div(class="item",
-      div(class="massive active header", "awesome-Rmarkdown"),
-      # div(class="menu",
-      #   a(class="item", href="#header", "Header"),
-      #   a(class="item", href="#divider", "Divider")
-      # ),
+    div(class="ui sidebar inverted vertical visible menu",
       div(class="item",
-        div(class="active header", "Intro"),
-        div(class="menu",
-          a(class="item", href="#basic", "Basic"),
-          a(class="item", href="#yaml", "YAML")
+        div(class="massive active header", h1("Awesome Rmarkdown")),
+        # div(class="menu",
+        #   a(class="item", href="#header", "Header"),
+        #   a(class="item", href="#divider", "Divider")
+        # ),
+        div(class="item",
+          div(class="active header", "Intro"),
+          div(class="menu",
+            a(class="item", href="#basic", "Basic"),
+            a(class="item", href="#yaml", "YAML")
+          )
+        ),
+        div(class="item",
+          div(class="active header", "Output Formats"),
+          div(class="menu",
+            a(class="item", href="#card", "PDF"),
+            a(class="item", href="#output-formats", "HTML")
+          )
+        ),
+        div(class="item",
+          a(href="#interactive-visuals", div(class="active header", "Interactive Visuals")),
+          div(class="menu",
+            a(class="item", href="#highcharter", "Highcharter")
+          )
+        ),
+        div(class="item",
+          div(class="active header", "UI Extensions"),
+          div(class="menu",
+            a(class="item", href="#semantic-ui", "Semantic UI"),
+            a(class="item", href="#sunburst", "Sunburst"),
+            a(class="item", href="#tabset", "...")
+          )
         )
       ),
       div(class="item",
-        div(class="active header", "Output Formats"),
-        div(class="menu",
-          a(class="item", href="#card", "PDF"),
-          a(class="item", href="#output-formats", "HTML")
-        )
-      ),
-      div(class="item",
-        a(href="#interactive-visuals", div(class="active header", "Interactive Visuals")),
-        div(class="menu",
-          a(class="item", href="#highcharter", "Highcharter")
-        )
-      ),
-      div(class="item",
-        div(class="active header", "UI Extensions"),
-        div(class="menu",
-          a(class="item", href="#semantic-ui", "Semantic UI"),
-          a(class="item", href="#sunburst", "Sunburst"),
-          a(class="item", href="#tabset", "...")
-        )
+        img(class="ui centered tiny image", src="images/ruserlogo_real.png")
       )
     )
-  ),
-  tags$script("$('.ui.labeled.icon.sidebar')
-          .sidebar('toggle')
-          ;")
   )
 }
