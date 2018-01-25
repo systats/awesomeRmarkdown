e90b0e2012c597c5957cd7fd7cd62b01476feb8a
Simon Roth <systats@users.noreply.github.com>
2018-01-25 18:07:12 +0100

    final

--- src/semantic_ui.R
+++ src/semantic_ui.R
@@ -65,6 +65,7 @@ repo_structure <- function(){
   )
 }
 
+
 sidebar <- function() {
   div(class="ui sidebar inverted vertical visible menu", 
     div(class="item",
@@ -76,31 +77,42 @@ sidebar <- function() {
       #   a(class="item", href="#divider", "Divider")
       # ),
       div(class="item",
-        div(class="active header", "Intro"),
+        a(class="active header", href="#reproducible-research", "Reproducible Research"),
+        br(),
+        a(class="active header", href="#rmarkdown", "(R) Markdown"),
         div(class="menu",
-          a(class="item", href="#basic", "Basic"),
-          a(class="item", href="#yaml", "YAML")
+          a(class="item", href="#yaml", "YAML"),
+          a(class="item", href="#headers", "Headers"),
+          a(class="item", href="#tabbed", "Tabbed Sections"),
+          a(class="item", href="#lists", "Lists"),
+          a(class="item", href="#highlight", "Text Highlighting"),
+          a(class="item", href="#formula", "Formula"),
+          a(class="item", href="#images", "Images"),
+          a(class="item", href="#quotes", "Quotes"),
+          a(class="item", href="#links", "Links"),
+          a(class="item", href="#themes", "Themes"),
+          a(class="item", href="#citations", "Citations")
         )
       ),
       div(class="item",
-        div(class="active header", "Output Formats"),
+        div(class="active header", "R Code"),
         div(class="menu",
-          a(class="item", href="#card", "PDF"),
-          a(class="item", href="#output-formats", "HTML")
+          a(class="item", href="#embed", "Embed Code"),
+          a(class="item", href="#emo", "Emo[ji]"),
+          a(class="item", href="#tables", "Tables"),
+          a(class="item", href="#html-widgets", "HTML Widgets")
         )
       ),
       div(class="item",
-        a(href="#interactive-visuals", div(class="active header", "Interactive Visuals")),
+        a(href="#projects", div(class="active header", "Standalone Projects")),
         div(class="menu",
           a(class="item", href="#highcharter", "Highcharter")
         )
       ),
       div(class="item",
-        div(class="active header", "UI Extensions"),
+        div(class="active header", "Advanced"),
         div(class="menu",
-          a(class="item", href="#semantic-ui", "Semantic UI"),
-          a(class="item", href="#sunburst", "Sunburst"),
-          a(class="item", href="#tabset", "...")
+          a(class="item", href="#semantic-ui", "Semantic UI")
         )
       )
     ),
